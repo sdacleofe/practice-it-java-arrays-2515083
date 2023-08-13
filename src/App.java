4e97b74439f8697ca0fd1d6f24514d3decf11a38
sdacleofe <103588804+sdacleofe@users.noreply.github.com>
2023-08-13 10:34:41 +0000

    Find the second smallest

--- src/App.java
+++ src/App.java
@@ -1,7 +1,40 @@
+import java.util.Arrays;
+
 public class App {
   
+  // solution method 1
+  public static Integer findSecondSmallestItem2(Integer[] arr) {
+    if (arr.length <= 1) {
+        return null;
+      }
+    Arrays.sort(arr);
+    for (int i=0; i<arr.length-1; i++) {
+      if (arr[i] != arr[i+1]) {
+        return arr[i+1];
+      }
+    }
+    return null;
+  }
+
   public static Integer findSecondSmallestItem(Integer[] arr) {
-    return 0;
+    int smallest = Integer.MAX_VALUE;
+    int secondSmallest = Integer.MAX_VALUE;
+
+    for (int i=0; i<arr.length; i++) {
+      int current = arr[i];
+      if (current < smallest) {
+        secondSmallest = smallest;
+        smallest = current;
+      } else if (current < secondSmallest && current != smallest) {
+        secondSmallest = current;
+      }
+    }
+
+    if ( secondSmallest == Integer.MAX_VALUE) {
+      return null;
+    }
+
+    return secondSmallest;
   }
     
   public static void main(String args[]) {

df1fc999441abb7f5c7eef3ed364cf39ac6b7b43
sdacleofe <103588804+sdacleofe@users.noreply.github.com>
2023-08-13 09:15:53 +0000

    Iterating through the Arrays

--- src/App.java
+++ src/App.java
@@ -2,5 +2,31 @@ public class App {
   
   public static void main(String[] args) {
     int[] arr = new int[]{92, 12, 38, 74};
+
+    int[] arr = new int[] { 92, 12, 38, 74 };
+    // iterate forward
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    // iterate backwards
+    for (int i = arr.length - 1; i >= 0; i--) {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    // another option use an enhance for loop or foreach
+    for (int item : arr) {
+      System.out.println(item);
+    }
+    System.out.println();
+
+    // another option use Java Streams (Newer Version)
+    Arrays.stream(arr).forEach(System.out::println);
+
+    // Arrays.stream(arr).filter()
+    // Arrays.stream(arr).average()
+    // Arrays.stream(arr).sum()
   }
 }
